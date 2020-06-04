6696bc41e489f10709238138e5734f2e75847475
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-04 01:06:24 -0500

    commit

---
+++ week-1/hello-world.js
@@ -0,0 +1,12 @@
+/* 
+============================================ 
+; Title: Exercise 1.3 
+; Author: Professor Krasso 
+; Date: 31 May 2020
+; Modified By: Nicole Barleta
+; Description: This program demonstrates the console.log
+;
+;=========================================== 
+*/ 
+
+console.log("Hello World, my name is Nicole Barleta")
