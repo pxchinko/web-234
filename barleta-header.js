8cdadf129896c67b9f49e12057769b37245a7e47
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-08 00:00:37 -0500

    commit

---
+++ barleta-header.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   7 June 2020
+; Modified By: Marie Nicole Barleta
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+exports.display = function (firstName, lastName, assignment) {
+	let output = '\n' + firstName + ' ' + lastName + '\n' + assignment + '\nDate: ' +
+	new Date().toLocaleDateString('en-US')
+
+  return output
+
+}
---
+++ week-2/barleta-header-test.js
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   7 June 2020
+; Modified By: Marie Nicole Barleta
+; Description: Test for a formatted header
+;===========================================
+*/
+
+// import statement
+const header = require('../barleta-header.js');
+
+console.log(header.display("Marie", "Barleta", "Exercise 2.2 - Header"));
+
