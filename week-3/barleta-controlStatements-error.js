98dd7905a7d784b1ee3be79845a8e23528114bc1
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-11 21:17:00 -0500

    commit

--- week-3/barleta-controlStatements-error.js
+++ week-3/barleta-controlStatements-error.js
@@ -11,23 +11,30 @@
 // import statement
 var header = require('../barleta-header.js');
 console.log(header.display("Marie", "Barleta", "Control Statements Find Errors"));
+console.log("\n")
 
 /*
+Find multiple errors.
+
 Expected Output:
-      *
-     ***
-    *****
-   *******
-  *********
+
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
 
 */
 
 //Start of code
 
-for (i = 1; i <= 5; i++){
-  if (i % 2 == 0) console.log("even");
-  else console.log("odd");
-
+for (var i=1; i <= 5; i+){
+  if (num % 2 != 0){
+    console.log(num + " is odd");
+  }
+  elseif {
+    console.log(x + " is even");
+  }
 }
 
 
