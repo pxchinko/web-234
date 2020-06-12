72d65640a8994f74aa75ca9695da14416be08f3d
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-11 21:40:11 -0500

    commit

--- week-3/alvarez-controlStatements-solution.js
+++ week-3/alvarez-controlStatements-solution.js
@@ -1,9 +1,9 @@
 
 /*=======================================
 ; Title: Discussion Board 3.1
-; Author: Professor Krasso
-; Date: 9 June 2020
-; Modify by: Karina Alvarez
+; Author: Karina Alvarez
+; Date: 11 June 2020
+; Modify by: Nicole Barleta
 ; Description: Javascript program using
 ; for statement. Three errors present.
 =======================================*/
@@ -13,10 +13,20 @@
 // two errors in the following statement
 let number = '';
 
-for (let i = 0; i < 11; i+)
+for (let i = 0; i < 11; i++){
 number += i + "\n";
 }
 
 // Output is numbers 0 - 10. Each in a new line.
 // One error present in the following statement.
-console.log(Number);
+console.log(number);
+
+
+/*
+My solution:
+
+1. Added an open curly bracket, for loop statement.
+2. Added a plus sign (+), to increment.
+3. Changed the Number variable in console log into small case 'n'.
+
+*/
