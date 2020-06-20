63a615a4992ff59d77b950d75b7ad15c6f254537
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-20 00:12:58 -0500

    commit

---
+++ week-4/barleta-exercise-4.3.js
@@ -0,0 +1,59 @@
+/*
+============================================
+; Title:  barleta-exercise-4.3.js
+; Author: Professor Krasso
+; Date:   15 June 2020
+; Modified By: Marie Nicole Barleta
+; Description:
+;===========================================
+*/
+
+// import statement
+var header = require('../barleta-header.js');
+console.log(header.display("Marie", "Barleta", "Exercise 4.3"));
+console.log("\n")
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 4.3
+  Today's Date
+  -- DISPLAYING ARRAY ITEMS --
+  Car
+  Truck
+  Motorcycle
+  Airplane
+  Bus
+  -- SELECTED VALUE --
+  Motorcycle
+  -- SELECTED VALUE --
+  Bus
+*/
+
+
+//start program
+var vehicles = ['Car', 'Truck', 'Motorcycle', 'Airplane', 'Bus']
+
+//function
+function getValue(arr, val){
+  for (var k=0; k < arr.length; k++){
+    if (arr[k] === val)
+      console.log(arr[k]);
+  }
+
+}
+
+//Output
+console.log(" -- DISPLAYING ARRAY ITEMS --");
+for (var x = 0; x < vehicles.length; x++){
+  console.log(vehicles[x])
+}
+
+console.log(" ")
+
+console.log(" -- SELECTED VALUE --");
+getValue(vehicles, "Motorcycle")
+
+console.log(" ")
+console.log(" -- SELECTED VALUE --");
+getValue(vehicles, "Bus")
