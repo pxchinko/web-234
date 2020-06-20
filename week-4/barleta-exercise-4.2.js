7ec16549a70591dfb8eed07aea51261639e63433
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-06-19 23:20:26 -0500

    commit

---
+++ week-4/barleta-exercise-4.2.js
@@ -0,0 +1,39 @@
+/*
+============================================
+; Title:  barleta-exercise-4.2.js
+; Author: Professor Krasso
+; Date:   19 June 2020
+; Modified By: Marie Nicole Barleta
+; Description: 1 dimensional array of 5 fruit elements
+;===========================================
+*/
+
+// import statement
+var header = require('../barleta-header.js');
+console.log(header.display("Marie", "Barleta", "Exercise 4.2"));
+console.log("\n")
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 4.2
+  Today's Date
+  Apple
+  Orange
+  Banana
+  Mango
+  Pear
+*/
+
+//start code
+var fruits = ['Apple', 'Orange', 'Banana', 'Mango', 'Pear'];
+
+//function
+function getFruit(arr){
+for (var k=0; k < arr.length; k++){
+console.log(arr[k])
+}
+}
+
+getFruit(fruits);
+
